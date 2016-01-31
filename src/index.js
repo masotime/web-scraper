6b2e2b9b83f03cef3ade2d7dea598af5d3762c5f
Benjamin Goh <masotime@gmail.com>
2016-01-31 15:05:26 -0800

    Partial work

---
+++ src/index.js
@@ -0,0 +1,90 @@
+import request from 'request';
+import fs from 'fs';
+import Promise from 'bluebird';
+import zlib from 'zlib';
+
+import { betterRequest, constructOptionsWithJar, constructError, constructResult, determineFilename } from './lib';
+
+// creates a closure instance of a scraper
+function factory() {
+
+	const jar = request.jar();
+	const constructOptions = (uri, { headers, query, body} ) => constructOptionsWithJar(uri, { headers, query, body, jar});
+
+	const get = (uri, { headers, query }) => new Promise( (resolve, reject) => {
+		const options = constructOptions(uri, { headers, query });
+		return betterRequest(options, (err, resp, body) => {
+			if (err) {
+				return reject(err);
+			} else if (resp.statusCode !== 200) {
+				return reject(constructError(options, resp, body));
+			} else {
+				return resolve(constructResult(body));
+			}
+		});
+
+	});
+
+	const post = (uri, { headers, query, body }) => new Promise( (resolve, reject) => {
+		const options = constructOptions({ headers, query, body });
+		return betterRequest(options, function(err, resp, body) {
+			if (err) {
+				return reject(err);
+			} else if (resp.statusCode !== 200) {
+				return reject(constructError(options, resp, body));
+			} else {
+				return resolve(constructResult(body));
+			}
+		});
+	});
+
+	// MAYDO: A download may be the response from a POST?
+	const download = function(uri, { headers, query, filename }) {
+
+		return determineFilename(uri, filename).then( downloadpath => {
+			console.log(`DOWNLOAD ${uri} to ${downloadpath}`);
+
+			const options = constructOptions({ headers, query });
+			const writeStream = fs.createWriteStream(downloadpath);
+			const req = request(options);
+
+			return new Promise((resolve, reject) => {
+				// adapted from http://nickfishman.com/post/49533681471/nodejs-http-requests-with-gzip-deflate-compression,
+				// but this time this is clearly a use case for streams
+				req.on('response', function(res) {
+					var encoding = res.headers['content-encoding'];
+
+					if (res.statusCode !== 200) {
+						return reject(constructError(options, res, res.body));
+					} else {
+						if (encoding === 'gzip') {
+							res.pipe(zlib.createGunzip()).pipe(writeStream);
+						} else if (encoding === 'deflate') {
+							res.pipe(zlib.createInflate()).pipe(writeStream);
+						} else {
+							res.pipe(writeStream);
+						}
+					}
+				});
+
+				req.on('error', function(err) {
+					return reject(err);
+				});
+
+				writeStream.on('error', function(err) {
+					return reject(err);
+				}).on('finish', function() {
+					writeStream.close();
+					return resolve(downloadpath);
+				});
+
+			});
+
+		});
+
+	};	
+
+	return { get, post };
+}
+
+module.exports = SessionFactory;
---
+++ src/lib.js
@@ -0,0 +1,143 @@
+import request from 'request';
+import cheerio from 'cheerio';
+import path from 'path';
+import fs from 'fs';
+import url from 'url';
+import Promise from 'bluebird';
+import zlib from 'zlib';
+
+// base options
+const BASE_OPTIONS = {
+	headers: {
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36",
+		"Cache-Control": "no-cache",
+		"Pragma": "no-cache"
+	}
+};
+
+// fix unicode in JSON response
+const UNICODE_HEADER = /\\x([0-9a-fA-F]{2})/g;
+
+// adds additional functionality like automatic gunzipping / deflating and 303 redirects
+// into mikeal's request.
+function betterRequest(options, callback) {
+
+	// my god why doesn't mikeal just bake this shit into request
+	const req = request(options);
+
+	// adapted from http://nickfishman.com/post/49533681471/nodejs-http-requests-with-gzip-deflate-compression
+	// TODO: Consider a streamed approach next time
+	req.on('response', function(res) {
+		const chunks = [];
+
+		res.on('data', function(chunk) {
+			chunks.push(chunk);
+		});
+
+		res.on('end', function() {
+			const buffer = Buffer.concat(chunks);
+			const encoding = res.headers['content-encoding'];
+
+			try {
+				if (encoding === 'gzip') {
+					console.log('Content is gzipped');
+					zlib.gunzip(buffer, (err, decoded) => callback(err, res, decoded && decoded.toString()));
+				} else if (encoding === 'deflate') {
+					console.log('Content is deflated');
+					zlib.inflate(buffer, (err, decoded) =>  callback(err, res, decoded && decoded.toString()));
+				} else {
+					// manually handle 303... bah
+					if (res.statusCode === 303) {
+						options.uri = res.headers.location;
+						return betterRequest(options, callback);
+					} else {
+						return callback(null, res, buffer && buffer.toString());
+					}
+				}
+			} catch (e) {
+				callback(e);
+			}
+
+		});
+
+	});
+
+	req.on('error', callback);
+}
+
+function constructError(options, resp, body) {
+	const error = new Error();
+	error.message = `${options.method || 'GET'} ERROR ${options.uri}`;
+	error.statusCode = resp.statusCode;
+	error.body = body;
+	return error;
+}
+
+// TODO: This could throw errors. Deal with it.
+function constructResult(resp, body) {
+	const result = { body };
+
+	// augment the result
+	switch (resp.mimeType) {
+		case 'text/html': result.$ = cheerio.load(body, { lowerCaseTags: true }); break;
+		case 'application/json': result.json = JSON.parse(body.replace(UNICODE_HEADER, (m, n) => String.fromCharCode(parseInt(n,16))));
+	}
+
+	return result;
+}
+
+function constructOptionsWithJar(uri, { headers, query, body, jar }) {
+	const options = { uri, jar };
+
+	options.headers = Object.assign({}, BASE_OPTIONS, headers);
+	if (query !== undefined) {
+		options.qs = query
+	}
+
+	if (body !== undefined) {
+		options.form = body;
+	}
+
+	return options;
+}
+
+function determineFilename(uri, filename) {
+	return new Promise((resolve, reject) => {
+		let baseFilename
+		try {
+			baseFilename = /[^\/]+$/.exec(url.parse(uri,true).pathname)[0];
+		} catch (err) {
+			console.warn(`WARNING Unable to determine base filename for ${uri}`);
+		}
+
+		// why is this the first condition? because we may need baseFilename if filename is a folder
+		if (!filename && !baseFilename) {
+			return reject(new Error(`DOWNLOAD ${uri} - Filename not given and cannot determine base name`)); // TODO: Nicer error
+		} else if (filename) {
+			// if the filename is actually a folder that already exists, then download to the folder using the baseFilename
+			fs.stat(filename, (err, result) => {
+				var finalValue;
+				try {
+					if (err || !result.isDirectory()) {
+						// just carry on using the filename
+						finalValue = filename;
+					} else {
+						// we append the basefilename to the directory
+						finalValue = path.join(filename, baseFilename);
+					}
+					return resolve(finalValue);
+				} catch (e) {
+					return reject(e);
+				}
+			});
+		} else {
+			// no filename, but we have a baseFilename
+			return resolve(baseFilename);
+		}
+
+	});
+}
+
+export default {
+	betterRequest, constructError, constructResult, constructOptionsWithJar, determineFilename
+};
